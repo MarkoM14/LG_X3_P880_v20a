361af15f3aa11a799c3b941e6a67995d56c2a172
Ming Lei <ming.lei@canonical.com>
2012-06-19 21:15:52 +0000

    usbnet: decrease suspend count if returning -EBUSY for runtime suspend
    This patch decreases dev->suspend_count in the -EBUSY failure path
    of usbnet_suspend. Without the change, the later runtime suspend
    will do nothing except for increasing dev->suspend_count.
    
    Signed-off-by: Ming Lei <ming.lei@canonical.com>
    Acked-by: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflicts:
    	drivers/net/usb/usbnet.c
    
    Change-Id: I12378764dc5fa84bee71c5e4ecd80a4cf9b6e9d4

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1503,6 +1503,7 @@ int usbnet_suspend (struct usb_interface *intf, pm_message_t message)
 		spin_lock_irq(&dev->txq.lock);
 		/* don't autosuspend while transmitting */
 		if (dev->txq.qlen && (message.event & PM_EVENT_AUTO)) {
+			dev->suspend_count--;
 			spin_unlock_irq(&dev->txq.lock);
 			return -EBUSY;
 		} else {
