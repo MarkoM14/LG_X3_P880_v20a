fcdebac0ed144cf891a8d7271f67915882ce6576
alfsamsung <alfsamsung@hotmail.com>
2014-08-10 20:15:45 +0200

    bcmdhd: fix to build for current linux version and cleanup board file
    Change-Id: Ib96d9da257467a6aae91f6ad67ef67c50bbc43b6
    
    bcmdhd: fix to build for current linux version and cleanup board file part 2. And remove old files.
    
    Change-Id: I5ad6213cbdf8f002016c835c8fc4660b60cf1c6e

--- arch/arm/mach-tegra/pm-t3.c
+++ arch/arm/mach-tegra/pm-t3.c
@@ -477,12 +477,7 @@ void tegra_lp0_cpu_mode(bool enter)
 #define PMC_DPD_SAMPLE			0x20
 
 struct tegra_io_dpd tegra_list_io_dpd[] = {
-	/* sd dpd bits in dpd2 register */
-#if 0
-	IO_DPD_INFO("sdhci-tegra.0",	1,	1), /* SDMMC1 */
-#endif
-	IO_DPD_INFO("sdhci-tegra.2",	1,	2), /* SDMMC3 */
-	IO_DPD_INFO("sdhci-tegra.3",	1,	3), /* SDMMC4 */
+/* Empty DPD list - sd dpd entries removed */
 };
 
 /* we want to cleanup bootloader io dpd setting in kernel */
@@ -514,10 +509,8 @@ void tegra_io_dpd_enable(struct tegra_io_dpd *hnd)
 	unsigned int dpd_status;
 	unsigned int dpd_enable_lsb;
 
-	if ((!hnd)) {
-		pr_warn("SD IO DPD handle NULL in %s\n", __func__);
+	if ((!hnd))
 		return;
-	}
 	spin_lock(&tegra_io_dpd_lock);
 	dpd_enable_lsb = (hnd->io_dpd_reg_index) ? APBDEV_DPD2_ENABLE_LSB :
 						APBDEV_DPD_ENABLE_LSB;
@@ -544,10 +537,8 @@ void tegra_io_dpd_disable(struct tegra_io_dpd *hnd)
 	unsigned int dpd_status;
 	unsigned int dpd_enable_lsb;
 
-	if ((!hnd)) {
-		pr_warn("SD IO DPD handle NULL in %s\n", __func__);
+	if ((!hnd))
 		return;
-	}
 	spin_lock(&tegra_io_dpd_lock);
 	dpd_enable_lsb = (hnd->io_dpd_reg_index) ? APBDEV_DPD2_ENABLE_LSB :
 						APBDEV_DPD_ENABLE_LSB;
