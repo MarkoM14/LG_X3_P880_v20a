77842b317bd4bac8fd8dcf30432c6a9006ab95b4
alfsamsung <alfsamsung@hotmail.com>
2015-10-27 16:42:23 +0100

    Revert "ts0710_mux: Back to original semaphore timeout for now"
    This reverts commit 655a4e2a7828b4f99a2aadfa94356d660d2cf045.

--- drivers/misc/ts0710mux/ts0710_mux.c
+++ drivers/misc/ts0710mux/ts0710_mux.c
@@ -2235,7 +2235,7 @@ static void ts_ldisc_clear_nodes(void)
 /*There is no need to wait forever for sema down
  *Add a timeout module for testing
  */
-unsigned long looper_timeout = 250;  // 100ms
+unsigned long looper_timeout = 10;  // 100ms
 module_param(looper_timeout, ulong, 0664);
 MODULE_PARM_DESC(looper_timeout,
         "ts0710 mux - tx_looper semaphore down timeout");
