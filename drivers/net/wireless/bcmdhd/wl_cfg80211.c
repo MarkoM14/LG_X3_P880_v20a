2cad5d61b5ffa0b4eaed9b390b107d56bb04d310
alfsamsung <alfsamsung@hotmail.com>
2015-12-09 03:07:11 +0100

    Revert "net: wireless: bcmdhd: TEST: Add WIPHY_WOWLAN_ANY support"
    This reverts commit 5598764f7f6182a1dcc9c7154ea169406058de18.

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -6885,14 +6885,14 @@ static s32 wl_setup_wiphy(struct wireless_dev *wdev, struct device *sdiofunc_dev
 	wdev->wiphy->flags |= WIPHY_FLAG_SUPPORTS_TDLS;
 #endif
 
-#if defined(CONFIG_PM)
+#if defined(CONFIG_PM) && defined(WL_CFG80211_P2P_DEV_IF)
 	/*
 	 * From linux-3.10 kernel, wowlan packet filter is mandated to avoid the
 	 * disconnection of connected network before suspend. So a dummy wowlan
 	 * filter is configured for kernels linux-3.8 and above.
 	 */
 	wdev->wiphy->wowlan.flags = WIPHY_WOWLAN_ANY;
-#endif /* CONFIG_PM //&& WL_CFG80211_P2P_DEV_IF */
+#endif /* CONFIG_PM && WL_CFG80211_P2P_DEV_IF */
 
 	WL_DBG(("Registering custom regulatory)\n"));
 	wdev->wiphy->flags |= WIPHY_FLAG_CUSTOM_REGULATORY;
